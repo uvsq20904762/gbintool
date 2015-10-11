9d215623a2bfbeba8ce18f0a283cf53480242709
Utilisateur <lucas.epinette@gmail.com>
2015-10-11 15:15:23 +0200

    commit cli

--- src/main/groovy/fr/uvsq/doosa/gradletuto/Main.groovy
+++ src/main/groovy/fr/uvsq/doosa/gradletuto/Main.groovy
@@ -1,12 +1,33 @@
 package fr.uvsq.doosa.gradletuto
 
-class Main {
-    static void main(def args) {
-        Main m = new Main()
-        println m.returnHello()
-    }
+def cli = new CliBuilder(usage: 'Main.groovy <action> [<options>] <entrée1>:<entrée2>:...')
 
-    String returnHello() {
+cli.with {
+  h(longOpt: 'help', 'show usage information')
+  action(longOpt: 'action','une action parmi help, version, metadata, cat, sample')
+  o(longOpt: 'output','destination ( sans option pour la sortie standard)')
+  csv(longOpt: 'csv','sortie au format CSV(défaut)')
+  parquet(longOpt: 'parquet','sortie au format Avro/Parquet')
+  s(longOpt: 'size', 'taille des fichiers de sortie')
+  n(longOpt: 'n1st', 'traite uniquement les n premiers objets')
+  r(longOpt: 'nrand', 'traite n objets au hasard')
+}
+
+def options = cli.parse(args)
+if(!options) {
+  return
+}
+
+// print usage if no argument is given
+if(options.h) {
+  cli.usage()
+}
+if(options.action){
+println returnHello()
+}
+
+String returnHello() {
         'Hello'
     }
-}
+	
+
